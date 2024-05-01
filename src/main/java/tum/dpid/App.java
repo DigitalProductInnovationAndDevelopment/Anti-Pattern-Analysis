4dccd09024cd1c8eb5fdc53f420707dc1367b470
fehmisener <fehmiisener@gmail.com>
2024-05-01 16:31:38 +0200

    build: initialize repository with maven

---
+++ src/main/java/tum/dpid/App.java
@@ -0,0 +1,13 @@
+package tum.dpid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
