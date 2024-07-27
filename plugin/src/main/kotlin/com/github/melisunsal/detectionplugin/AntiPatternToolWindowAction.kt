1fb6a76443a3587331958139f934738734b9301b
Fehmi <58306766+fehmisener@users.noreply.github.com>
2024-07-27 14:54:27 +0200

    💽 feat(plugin): inital commit of the plugin (#43)

---
+++ plugin/src/main/kotlin/com/github/melisunsal/detectionplugin/AntiPatternToolWindowAction.kt
@@ -0,0 +1,12 @@
+package com.github.melisunsal.detectionplugin
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.wm.ToolWindowManager
+
+class AntiPatternToolWindowAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Anti Pattern Analysis")
+        toolWindow?.show(null)
+    }
+}
