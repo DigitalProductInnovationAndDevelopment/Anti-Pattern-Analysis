984bc224458c6fed4293969c3b4cc084790893e6
Melis Ece Unsal <melis.uensal@sixt.com>
2024-07-30 23:46:00 +0200

    add image

--- plugin/src/main/kotlin/com/github/melisunsal/detectionplugin/AntiPatternToolWindowFactory.kt
+++ plugin/src/main/kotlin/com/github/melisunsal/detectionplugin/AntiPatternToolWindowFactory.kt
@@ -1,18 +1,37 @@
 package com.github.melisunsal.detectionplugin
+
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import java.awt.*
+import java.awt.image.BufferedImage
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import javax.imageio.ImageIO
 import javax.swing.*
+
 
 class AntiPatternToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val panel = JPanel()
         panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
 
-        val infoLabel = JLabel("<html><h2>Anti Pattern Analysis</h2>" +
-                "<p>This plugin detects various anti-patterns in your Java code.</p>" +
+        val titleLabel = JLabel("<html><h2>Anti Pattern Analysis</h2></html>")
+        panel.add(titleLabel)
+
+        val imageUrl = this::class.java.getResource("/icons/logo.png")
+        val imageIcon = ImageIcon(imageUrl).image
+        val resizedImage = imageIcon.getScaledInstance(100, 100, Image.SCALE_DEFAULT)
+        val resizedIcon = ImageIcon(resizedImage)
+        val imageLabel = JLabel(resizedIcon)
+        panel.add(imageLabel)
+
+        // Add information label
+        val infoLabel = JLabel(
+                "<html><p>This plugin detects various anti-patterns in your Java code.</p>" +
                 "<p><strong>Detectable Anti-patterns:</strong></p>" +
                 "<ul><li>For Loop Database Anti-Performance</li></ul></html>")
         panel.add(infoLabel)
